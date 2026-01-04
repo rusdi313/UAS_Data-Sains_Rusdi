b15d2dc625d62ba96e17d833c5532f3d99f0e7eb
rusdi313 <rusdiaulia313@gmail.com>
2026-01-04 14:35:18 +0700

    Fix allowedHosts vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,5 +6,13 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000, // Kita set port default ke 3000 agar mirip CRA
+  },
+  preview: {
+    allowedHosts: true, 
+    host: true, // Ini penting agar bisa diakses dari luar container
+    port: 4173  // Port default preview vite
+  },
+  server: {
+    host: true // Ini untuk mode dev local
   }
 })
